ee62b72980c9e1432dd72989145dbab01cefe929
Mart0808DK <123973276+Mart0808DK@users.noreply.github.com>
2024-11-07 21:55:54 +0100

    removeTask made

--- src/components/KabanBoard.vue
+++ src/components/KabanBoard.vue
@@ -6,7 +6,7 @@
                 <div class="mt-4">
                     <draggable :list="column.tasks" class="flex flex-col" group="tasks" ghost-class="ghost-card" @change="onTaskChange">
                         <template #item="{ element: task }">
-                            <TaskCard :task="task" />
+                            <TaskCard :task="task" @remove-task="removeTask" />
                         </template>
                     </draggable>
                 </div>
@@ -15,8 +15,8 @@
                 </div>
             </div>
             <div class="">
-              <button @click="showModal = true" class="bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded">Add Column</button>
-            <AddColumnForm v-if="showModal" @add-column="handleAddColumn" @close="showModal = false" />
+                <button @click="showModal = true" class="bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 rounded">Add Column</button>
+                <AddColumnForm v-if="showModal" @add-column="handleAddColumn" @close="showModal = false" />
             </div>
         </div>
     </div>
@@ -30,6 +30,7 @@ import draggable from "vuedraggable";
 interface Task {
     id: number;
     title: string;
+    description: string;
     date: string;
     type?: string;
 }
@@ -57,217 +58,243 @@ export default defineComponent({
     components: {
         TaskCard,
         draggable,
-        AddColumnForm, 
+        AddColumnForm,
     },
     setup() {
         let showModal = ref(false);
-        
-        
+
         const columns = ref<Column[]>([]);
 
-        // function handleAddTask(columnId: number) {
-        //     columns.value[columnId-1].tasks.push({
-        //         title: "New Task",
-        //         date: "Sep 16",
-        //         type: "Feature Request",
-        //     });
-          
-        // }
+        
 
-         function handleAddColumn(title: string) {
-          const newId = columns.value.length > 0 ? Math.max(...columns.value.map(c => c.id)) + 1 : 1;
+        onMounted(() => {
+            const storedColumns = localStorage.getItem("columns");
+            if (storedColumns) {
+                columns.value = JSON.parse(storedColumns);
+            } else {
+                columns.value = [
+                    {
+                        id: 1,
+                        title: "Product Backlog",
+                        tasks: [
+                            {
+                                id: 1,
+                                title: "Add discount code to checkout page",
+                                description: "Add a field for users to enter a discount code on the checkout page.",
+                                date: "Sep 14",
+                                type: "Feature Request",
+                            },
+                            {
+                                id: 2,
+                                title: "Provide documentation on integrations",
+                                description: "Write documentation on how to integrate with our API.",
+                                date: "Sep 12",
+                                type: "Backend",
+                            },
+                            {
+                                id: 3,
+                                title: "Design shopping cart dropdown",
+                                description: "Design a dropdown for the shopping cart in the header.",
+                                date: "Sep 9",
+                                type: "Design",
+                            },
+                        ],
+                    },
+                    {
+                        id: 2,
+                        title: "In Progress",
+                        tasks: [
+                            {
+                                id: 4,
+                                title: "Redesign landing page",
+                                description: "Create a new design for the landing page.",
+                                date: "Sep 10",
+                                type: "Design",
+                            },
+                            {
+                                id: 5,
+                                title: "Implement user profile page",
+                                description: "Create a user profile page with user information.",
+                                date: "Sep 11",
+                                type: "Frontend",
+                            },
+                            {
+                                id: 6,
+                                title: "Fix bugs in user profile page",
+                                description: "Fix bugs in the user profile page.",
+                                date: "Sep 11",
+                                type: "Bug",
+                            },
+                        ],
+                    },
+                    {
+                        id: 3,
+                        title: "Blocked",
+                        tasks: [
+                            {
+                                id: 7,
+                                title: "Add payment gateway integration",
+                                description: "Integrate a payment gateway for processing payments.",
+                                date: "Sep 13",
+                                type: "Backend",
+                            },
+                            {
+                                id: 8,
+                                title: "Update product images",
+                                description: "Update product images on the website.",
+                                date: "Sep 12",
+                                type: "Design",
+                            },
+                            {
+                                id: 9,
+                                title: "Optimize site performance",
+                                description: "Optimize",
+                                date: "Sep 14",
+                                type: "Backend",
+                            },
+                            {
+                                id: 10,
+                                title: "Review code for security vulnerabilities",
+                                description: "Review code for security vulnerabilities.",
+                                date: "Sep 15",
+                                type: "Backend",
+                            },
+                            {
+                                id: 11,
+                                title: "Update product descriptions",
+                                description: "Update product descriptions on the website.",
+                                date: "Sep 13",
+                                type: "Content",
+                            },
+                            {
+                                id: 12,
+                                title: "Add new feature to product page",
+                                description: "Add a new feature to the product page.",
+                                date: "Sep 15",
+                                type: "Feature Request",
+                            },
+                        ],
+                    },
+                    {
+                        id: 4,
+                        title: "Review",
+                        tasks: [
+                            {
+                                id: 13,
+                                title: "Fix bugs in user profile page",
+                                description: "Fix bugs in the user profile page.",
+                                date: "Sep 11",
+                                type: "Bug",
+                            },
+                            {
+                                id: 14,
+                                title: "Update product images",
+                                description: "Update product images on the website.",
+                                date: "Sep 12",
+                                type: "Design",
+                            },
+                            {
+                                id: 15,
+                                title: "Optimize site performance",
+                                description: "Optimize",
+                                date: "Sep 14",
+                                type: "Backend",
+                            },
+                            {
+                                id: 15,
+                                title: "Review code for security vulnerabilities",
+                                description: "Review code for security vulnerabilities.",
+                                date: "Sep 15",
+                                type: "Backend",
+                            },
+                        ],
+                    },
+                    {
+                        id: 5,
+                        title: "Done",
+                        tasks: [
+                            {
+                                id: 16,
+                                title: "Provide documentation on integrations",
+                                description: "Write documentation on how to integrate with our API.",
+                                date: "Sep 12",
+                                type: "Backend",
+                            },
+                            {
+                                id: 17,
+                                title: "Design shopping cart dropdown",
+                                description: "Design a dropdown for the shopping cart in the header.",
+                                date: "Sep 9",
+                                type: "Design",
+                            },
+                        ],
+                    },
+                ];
+            }
+        });
+
+        const removeTask = (taskId: number) => {
+            for (const column of columns.value) {
+                const taskIndex = column.tasks.findIndex(task => task.id === taskId);
+                if (taskIndex !== -1) {
+                    column.tasks.splice(taskIndex, 1);
+                    break;
+                }
+            }
+        };
+        function handleAddTask(columnId: number) {
+            const newId = columns.value[columnId - 1].tasks.length > 0 ? Math.max(...columns.value[columnId - 1].tasks.map(t => t.id)) + 1 : 1;
+            columns.value[columnId - 1].tasks.push({
+                id: newId,
+                title: "New Task",
+                description: "Description of the task",
+                date: "Sep 15",
+            });
+        }
+
+        function handleAddColumn(title: string) {
+            const newId = columns.value.reduce((maxId, column) => Math.max(maxId, column.id), 0) + 1;
             columns.value.push({
                 id: newId,
                 title: title,
                 tasks: [],
             });
             showModal.value = false;
-         }
+        }
 
-         const onTaskChange = (event: DraggableEvent) => {
+        const onTaskChange = (event: DraggableEvent) => {
             if (event.removed) {
                 const task = event.removed.element;
                 const oldIndex = event.removed.oldIndex;
-                const columnId = columns.value.findIndex((column) => column.tasks.includes(task));
+                const columnId = columns.value.findIndex(column => column.tasks.includes(task));
                 columns.value[columnId].tasks.splice(oldIndex, 1);
             }
             if (event.added) {
                 const task = event.added.element;
                 const newIndex = event.added.newIndex;
-                const columnId = columns.value.findIndex((column) => column.tasks.includes(task));
+                const columnId = columns.value.findIndex(column => column.tasks.includes(task));
                 columns.value[columnId].tasks.splice(newIndex, 0, task);
             }
             console.log("Columns", columns.value);
         };
-            
-          
-    
-            onMounted(() => {
-                const storedColumns = localStorage.getItem("columns");
-                if (storedColumns) {
-                    columns.value = JSON.parse(storedColumns);
-                } else {
-                    columns.value = [
-                        {
-                            id: 1,
-                            title: "Product Backlog",
-                            tasks: [
-                                {
-                                    id: 1,
-                                    title: "Add discount code to checkout page",
-                                    date: "Sep 14",
-                                    type: "Feature Request",
-                                },
-                                {
-                                    id: 2,
-                                    title: "Provide documentation on integrations",
-                                    date: "Sep 12",
-                                    type: "Backend",
-                                },
-                                {
-                                    id: 3,
-                                    title: "Design shopping cart dropdown",
-                                    date: "Sep 9",
-                                    type: "Design",
-                                },
-                            ],
-                        },
-                        {
-                            id: 2,
-                            title: "In Progress",
-                            tasks: [
-                                {
-                                    id: 1,
-                                    title: "Redesign landing page",
-                                    date: "Sep 10",
-                                    type: "Design",
-                                },
-                                {
-                                    id: 2,
-                                    title: "Implement user profile page",
-                                    date: "Sep 11",
-                                    type: "Frontend",
-                                },
-                                {
-                                    id: 3,
-                                    title: "Fix bugs in user profile page",
-                                    date: "Sep 11",
-                                    type: "Bug",
-                                },
-                            ],
-                        },
-                        {
-                            id: 3,
-                            title: "Blocked",
-                            tasks: [
-                                {
-                                    id: 1,
-                                    title: "Add payment gateway integration",
-                                    date: "Sep 13",
-                                    type: "Backend",
-                                },
-                                {
-                                    id: 2,
-                                    title: "Update product images",
-                                    date: "Sep 12",
-                                    type: "Design",
-                                },
-                                {
-                                    id: 3,
-                                    title: "Optimize site performance",
-                                    date: "Sep 14",
-                                    type: "Backend",
-                                },
-                                {
-                                    id: 4,
-                                    title: "Review code for security vulnerabilities",
-                                    date: "Sep 15",
-                                    type: "Backend",
-                                },
-                                {
-                                    id: 5,
-                                    title: "Update product descriptions",
-                                    date: "Sep 13",
-                                    type: "Content",
-                                },
-                                {
-                                    id: 6,
-                                    title: "Add new feature to product page",
-                                    date: "Sep 15",
-                                    type: "Feature Request",
-                                },
-                            ],
-                        },
-                        {
-                            id: 4,
-                            title: "Review",
-                            tasks: [
-                                {
-                                    id: 1,
-                                    title: "Fix bugs in user profile page",
-                                    date: "Sep 11",
-                                    type: "Bug",
-                                },
-                                {
-                                    id: 2,
-                                    title: "Update product images",
-                                    date: "Sep 12",
-                                    type: "Design",
-                                },
-                                {
-                                    id: 3,
-                                    title: "Optimize site performance",
-                                    date: "Sep 14",
-                                    type: "Backend",
-                                },
-                                {
-                                    id: 4,
-                                    title: "Review code for security vulnerabilities",
-                                    date: "Sep 15",
-                                    type: "Backend",
-                                },
-                            ],
-                        },
-                        {
-                            id: 5,
-                            title: "Done",
-                            tasks: [
-                                {
-                                    id: 1,
-                                    title: "Provide documentation on integrations",
-                                    date: "Sep 12",
-                                    type: "Backend",
-                                },
-                                {
-                                    id: 2,
-                                    title: "Design shopping cart dropdown",
-                                    date: "Sep 9",
-                                    type: "Design",
-                                },
-                            ],
-                        },
-                    ];
-                }
-            });
 
-            
-    
-            watch(columns, (newColumns) => {
+        watch(
+            columns,
+            newColumns => {
                 localStorage.setItem("columns", JSON.stringify(newColumns));
-            }, { deep: true });
-    
-            return {
-                columns,
-                // handleAddTask,
-                onTaskChange,
-                showModal,
-                handleAddColumn,
-            };
-        },
+            },
+            { deep: true }
+        );
 
-    
-
-    
+        return {
+            columns,
+            handleAddTask,
+            onTaskChange,
+            showModal,
+            handleAddColumn,
+            removeTask,
+        };
+    },
 });
 </script>
 
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -3,14 +3,19 @@
         <div class="flex justify-between">
             <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">{{ task.title }}</p>
         </div>
+        <p class="text-gray-600 text-sm mt-2">{{ task.description }}</p>
         <div class="flex mt-4 justify-between items-center">
             <span class="text-sm text-gray-600">{{ task.date }}</span>
             <span class="text-sm text-gray-600">{{ task.type }}</span>
         </div>
+        <div class="flex justify-between">
+            <button class="bg-gray-500 hover:bg-blue-700 text-white font-semi py-2 px-4 rounded mt-4">Edit</button>
+            <button @click="removeTask(task.id)" class="bg-gray-400 hover:bg-red-700 text-white font-semi py-2 px-4 rounded mt-4">Delete</button>
+        </div>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -21,5 +26,17 @@ export default defineComponent({
             required: true,
         },
     },
+    setup(_, { emit }) {
+        const removeTask = (id: number) => {
+            emit("remove-task", id);
+        };
+
+        return {
+            removeTask,
+        };
+    },
 });
+
+
+
 </script>
